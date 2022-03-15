c6702a80b2336b3095d0761f41f1a846539ef108
Lassi Köykkä <lassi.koykka@talentree.fi>
2022-03-15 16:27:51 +0200

    Updated xkcd func

--- functions.zsh
+++ functions.zsh
@@ -5,13 +5,25 @@ function cheat () {
     curl -s cheat.sh/$str | less
 }
 
-function xkcd () {
+function rndXkcdUrl () {
     newestNum=$(curl -s https://xkcd.com/info.0.json | jq .num)
     rnumber=$((1 + $RANDOM % $newestNum))
     json=$(curl -s https://xkcd.com/$rnumber/info.0.json)
     url=$(jq -r .img <<< $json)
     echo $url
+}
+
+function xkcd () {
+    newestNum=$(curl -s https://xkcd.com/info.0.json | jq .num)
+    rnumber=$((1 + $RANDOM % $newestNum))
+    json=$(curl -s https://xkcd.com/$rnumber/info.0.json)
+    url=$(jq -r .img <<< $json)
     title=$(jq -r .title <<< $json)
-    echo "\n$title\n"
-    icat --align left $url
+    alt=$(jq -r .alt <<< $json)
+    transcript=$(jq -r .transcript <<< $json)
+    date="$(jq -r .day <<< $json).$(jq -r .month <<< $json).$(jq -r .year <<< $json)"
+    echo $url
+    echo "\n\"$title\"\n"
+    echo "#$rnumber -- $date\n"
+    icat --align left $url || (echo $alt && echo $transcript)
 }
