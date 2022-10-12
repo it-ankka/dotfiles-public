2eb89c937e781b163a1f96ea6851b4d99f04b0aa
Lassi Köykkä <lassi_koykka@hotmail.com>
2022-10-12 21:46:12 +0300

    Updated formatting commands in nvim

--- nvim/plugins/neoformat.lua
+++ nvim/plugins/neoformat.lua
@@ -1,5 +1,5 @@
 local Plug = vim.fn['plug#']
 Plug 'sbdchd/neoformat'
 
-vim.api.nvim_set_keymap("n", "<leader>pF", "<cmd>Neoformat<CR>", {noremap = true})
-vim.api.nvim_set_keymap("v", "<leader>pF", "<cmd>Neoformat<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>pf", "<cmd>Neoformat<CR>", {noremap = true})
+vim.api.nvim_set_keymap("v", "<leader>pf", "<cmd>Neoformat<CR>", {noremap = true})
