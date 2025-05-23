69f72cec3863fda40d37b3205f2dce221e166282
It Ankka <github@cote.mozmail.com>
2025-05-23 22:11:56 +0300

    Update dotfiles

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -22,10 +22,10 @@ if vim.fn.has('wsl') == 1 then
       ['+'] = 'clip.exe',
       ['*'] = 'clip.exe',
     },
-    paste = {
-      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
+    -- paste = {
+    --   ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    --   ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    -- },
     cache_enabled = 0,
   }
 end
