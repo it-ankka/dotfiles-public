4c2538fe2397c300d1ab03dde785f1a8b0b5fac2
It Ankka <github@cote.mozmail.com>
2025-05-26 16:23:54 +0300

    Remove unnecessary keybinds from Aerial.nvim

--- nvim/lua/plugin/aerial.lua
+++ nvim/lua/plugin/aerial.lua
@@ -1,7 +1,5 @@
-require("aerial").setup({filter_kind = false})
+require("aerial").setup({ filter_kind = false })
 
 vim.api.nvim_set_keymap("n", "{", "<cmd>AerialPrev<CR>", {})
 vim.api.nvim_set_keymap("n", "}", "<cmd>AerialNext<CR>", {})
-vim.api.nvim_set_keymap("n", "[[", "<cmd>AerialPrevUp<CR>", {})
-vim.api.nvim_set_keymap("n", "]]", "<cmd>AerialNextUp<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>A", "<cmd>AerialToggle! left<CR>", {})
