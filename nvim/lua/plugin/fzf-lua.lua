72f948bd2a1dbd91d6ba5a49444cf037f1376a4a
It Ankka <github@cote.mozmail.com>
2025-05-26 16:24:07 +0300

    Update fzf-lua keybinds

--- nvim/lua/plugin/fzf-lua.lua
+++ nvim/lua/plugin/fzf-lua.lua
@@ -1,49 +1,87 @@
-local function Files()
-  return (require("fzf-lua")).files({sync = true, fd_opts = "--type f --hidden --exclude node_modules --exclude .git", find_opts = "-type f -not -path '*/node_modules/*' -printf '%P'"})
+local fzf_lua = require("fzf-lua")
+
+local Files = function()
+  return fzf_lua.files({
+    fd_opts = "--type f --hidden --exclude node_modules --exclude .git",
+    find_opts = "-type f -not -path '*/node_modules/*' -printf '%P'",
+    git_icons = true
+  })
 end
 
-
-local function AllFiles()
-  return (require("fzf-lua")).files({sync = true, fd_opts = "--type f --hidden --exclude .git", find_opts = "-type f -not -path '*/.git/*' -printf '%P'", git_icons = false, file_icons = false})
+local Buffers = function()
+  return fzf_lua.buffers({
+    git_icons = true
+  })
 end
 
-local function GrepProject()
-  return (require("fzf-lua")).grep_project({fzf_opts = {["--delimiter"] = "'[:]'"}})
+local AllFiles = function()
+  return fzf_lua.files({
+    fd_opts = "--type f --hidden --exclude .git",
+    find_opts = "-type f -not -path '*/.git/*' -printf '%P'",
+    git_icons = true,
+  })
 end
 
-local function GrepAll()
-  return (require("fzf-lua")).grep({cmd = "rg -uu --color=always --smart-case -g !{.git}", git_icons = false, file_icons = false})
+local GrepProject = function()
+  return fzf_lua.grep_project({
+    fzf_opts = { ["--delimiter"] = "'[:]'" }
+  })
 end
 
-
-local function Diagnostics()
-  return (require("fzf-lua")).lsp_document_diagnostics({sync = true, jump_to_single_result = true})
+local GrepAll = function()
+  return fzf_lua.grep({
+    cmd = "rg -uu --color=always --smart-case -g !{.git}",
+    git_icons = false,
+    file_icons = false
+  })
 end
 
+local Resume = function() return fzf_lua.resume() end
 
-local function AllDiagnostics()
-  return (require("fzf-lua")).lsp_workspace_diagnostics({sync = true, jump_to_single_result = true})
-end
+local AllDiagnostics = function() return fzf_lua.lsp_workspace_diagnostics() end
+local AllSymbols = function() return fzf_lua.lsp_live_workspace_symbols() end
+local CodeActions = function() fzf_lua.lsp_code_actions({ jump1 = false }) end
+local Declarations = function() fzf_lua.lsp_declarations() end
+local Definitions = function() fzf_lua.lsp_definitions() end
+local Diagnostics = function() return fzf_lua.lsp_document_diagnostics() end
+local References = function() fzf_lua.lsp_references() end
+local Symbols = function() return fzf_lua.lsp_document_symbols() end
+local TypeDefinitions = function() fzf_lua.lsp_typedefs() end
 
-vim.api.nvim_create_user_command("Files", Files, {})
+vim.api.nvim_create_user_command("Resume", Resume, {})
+
 vim.api.nvim_create_user_command("AllFiles", AllFiles, {})
-vim.api.nvim_create_user_command("GrepProject", GrepProject, {})
-vim.api.nvim_create_user_command("GrepAll", GrepAll, {})
+vim.api.nvim_create_user_command("AllSymbols", AllSymbols, {})
+vim.api.nvim_create_user_command("CodeActions", CodeActions, {})
+vim.api.nvim_create_user_command("Declarations", Declarations, {})
+vim.api.nvim_create_user_command("Definitions", Definitions, {})
 vim.api.nvim_create_user_command("Diagnostics", Diagnostics, {})
+vim.api.nvim_create_user_command("References", References, {})
+vim.api.nvim_create_user_command("Symbols", Symbols, {})
+vim.api.nvim_create_user_command("TypeDefinitions", TypeDefinitions, {})
+
 vim.api.nvim_create_user_command("AllDiagnostics", AllDiagnostics, {})
+vim.api.nvim_create_user_command("Buffers", Buffers, {})
+vim.api.nvim_create_user_command("Files", Files, {})
+vim.api.nvim_create_user_command("GrepAll", GrepAll, {})
+vim.api.nvim_create_user_command("GrepProject", GrepProject, {})
 
 -- KEYMAPS
-vim.keymap.set({"n"}, "<c-P>", "<cmd>AllFiles<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>F", "<cmd>AllFiles<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>f", "<cmd>Files<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>R", "<cmd>GrepAll<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>r", "<cmd>GrepProject<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>b", "<cmd>lua require('fzf-lua').buffers()<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>gr", "<cmd>lua require('fzf-lua').lsp_references({ sync = true, jump_to_single_result = true})<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>gd", "<cmd>lua require('fzf-lua').lsp_definitions({ sync = true, jump_to_single_result = true})<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>gD", "<cmd>lua require('fzf-lua').lsp_declarations({ sync = true, jump_to_single_result = true})<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>gy", "<cmd>lua require('fzf-lua').lsp_typedefs({ sync = true, jump_to_single_result = true})<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>ga", "<cmd>lua require('fzf-lua').lsp_code_actions()<CR>", {silent = true})
-vim.keymap.set({"n"}, "<leader>gq", "<cmd>AllDiagnostics<CR>", {silent = true})
+vim.keymap.set({ "n" }, "<c-P>", AllFiles, { silent = true })
+vim.keymap.set({ "n" }, "<leader>F", Resume, { silent = true })
+vim.keymap.set({ "n" }, "<leader>fF", AllFiles, { silent = true })
+vim.keymap.set({ "n" }, "<leader>ff", Files, { silent = true })
+vim.keymap.set({ "n" }, "<leader>fR", GrepAll, { silent = true })
+vim.keymap.set({ "n" }, "<leader>fr", GrepProject, { silent = true })
+vim.keymap.set({ "n" }, "<leader>fb", Buffers, { silent = true })
 
-require("fzf-lua").setup({winopts = {width = 0.8, height = 0.8}})
+vim.keymap.set({ "n" }, "<leader>gD", Declarations, { silent = true })
+vim.keymap.set({ "n" }, "<leader>ga", CodeActions, { silent = true })
+vim.keymap.set({ "n" }, "<leader>gd", Definitions, { silent = true })
+vim.keymap.set({ "n" }, "<leader>gs", Symbols, { silent = true })
+vim.keymap.set({ "n" }, "<leader>gS", AllSymbols, { silent = true })
+vim.keymap.set({ "n" }, "<leader>gr", References, { silent = true })
+vim.keymap.set({ "n" }, "<leader>gy", TypeDefinitions, { silent = true })
+vim.keymap.set({ "n" }, "<leader>gq", AllDiagnostics, { silent = true })
+
+require("fzf-lua").setup({ winopts = { width = 0.9, height = 0.9 } })
