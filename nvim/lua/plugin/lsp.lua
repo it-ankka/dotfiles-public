f74212a137ae13f7650d3a13fe850cb9ee71807a
It Ankka <github@cote.mozmail.com>
2025-11-27 15:27:35 +0200

    Simplify Roslyn lsp config

--- nvim/lua/plugin/lsp.lua
+++ nvim/lua/plugin/lsp.lua
@@ -212,31 +212,11 @@ vim.lsp.config("emmet_ls", {
 vim.lsp.config("roslyn", {
   settings = {
     ["csharp|background_analysis"] = {
-      dotnet_analyzer_diagnostics_scope = "fullSolution",
-      dotnet_compiler_diagnostics_scope = "fullSolution"
-    },
-    ["csharp|inlay_hints"] = {
-      csharp_enable_inlay_hints_for_implicit_object_creation = true,
-      csharp_enable_inlay_hints_for_implicit_variable_types = true,
-      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-      csharp_enable_inlay_hints_for_types = true,
-      dotnet_enable_inlay_hints_for_indexer_parameters = true,
-      dotnet_enable_inlay_hints_for_literal_parameters = true,
-      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-      dotnet_enable_inlay_hints_for_other_parameters = true,
-      dotnet_enable_inlay_hints_for_parameters = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-    },
-    ["csharp|code_lens"] = {
-      dotnet_enable_references_code_lens = true,
+      dotnet_analyzer_diagnostics_scope = "openFiles",
+      dotnet_compiler_diagnostics_scope = "openFiles"
     },
   },
-  filewatching = true
 })
-
-vim.lsp.enable("roslyn")
 
 --------------------
 -- ENABLE SERVERS --
